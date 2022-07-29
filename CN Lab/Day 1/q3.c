23048e705aee2398658f39827096a24cc5cc8433
Om Shree <om22shree@gmail.com>
2022-07-29 09:28:39 +0530

    few edits here and there

--- CN Lab/Day 1/q3.c
+++ CN Lab/Day 1/q3.c
@@ -2,13 +2,13 @@
 
 int main () {
     unsigned char bytes[4];
-    unsigned long n = 175;
+    unsigned long n = 983475;
 
     bytes[0] = (n >> 24) & 0xFF;
     bytes[1] = (n >> 16) & 0xFF;
     bytes[2] = (n >> 8) & 0xFF;
     bytes[3] = n & 0xFF;
     printf("%x %x %x %x\n", bytes[0], bytes[1], bytes[2], bytes[3]);
-    
+
     return 0;
 }
