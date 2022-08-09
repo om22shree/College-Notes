255d2763665c63432debb2f9625e1d6864a6493b
Om Shree <om22shree@gmail.com>
2022-08-09 16:59:02 +0530

    day 2 complete

--- CN Lab/Day 2/client.c
+++ CN Lab/Day 2/client.c
@@ -1,39 +1,38 @@
-#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #define PORT 8080
 
-
 int main() {
-    int clientfd;
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("Value of sockfd: %d\n", sockfd);
+	int sock = 0, valread, client_fd;
+	struct sockaddr_in serv_addr;
+	char* hello = "Hello from client. My name is Om Shree";
+	char buffer[1024] = { 0 };
 
-    struct sockaddr_in my_addr, serv_addr;
-    char *intro = "Hello, this is Om Shree";
-    char buffer[1024] = {0};
-    
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		printf("Socket creation error\n");
+	} else {
+        printf("Clientfd: %d\n", sock);
+    }
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+	serv_addr.sin_port = htons(PORT);
 
-    int bin = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
-    if(bin == -1) {
-        printf("Conversion falied\n");
-    } else {
-        printf("Conversion successful\n");
-    }
-    if ((clientfd = connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) == -1) {
-        printf("Connection Failed \n");
-    }
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+		printf("Invalid address\n");
+	}
 
-    send(sockfd, intro, strlen(intro), 0);
-    printf("Introduction sent\n");
-    int valread = read(sockfd, buffer, 1024);
-    printf("%s\n", buffer);
-  
-    close(clientfd);
-    return 0;
+	if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+		printf("\nConnection Failed \n");
+	}
+
+	send(sock, hello, strlen(hello), 0);
+	printf("Hello message sent from client\n");
+	valread = read(sock, buffer, 1024);
+	printf("Message Received from server:-\n%s\n", buffer);
+
+
+	close(client_fd);
+	return 0;
 }
--- CN Lab/Day 2/server.c
+++ CN Lab/Day 2/server.c
@@ -6,39 +6,48 @@
 #include <unistd.h>
 #define PORT 8080
 
-
 int main() {
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    printf("Value of sockfd: %d\n", sockfd);
+	int serverfd, chat, valread;
+	struct sockaddr_in address;
+	int opt = 1;
+	int addrlen = sizeof(address);
+	char buffer[1024] = { 0 };
+	char* hello = "Hello from server. My name is Om Shree";
 
-    struct sockaddr_in my_addr = {
-    my_addr.sin_family = AF_INET,
-    my_addr.sin_port = htons(PORT),
-    my_addr.sin_addr.s_addr = INADDR_ANY
-    };
-    int addrlen = sizeof(my_addr);
 
-    int bindval = bind(sockfd, (struct sockaddr*)&my_addr, sizeof(my_addr));
-    if(bindval == -1) {
-        printf("Binding failure\n");
-    } else {
-        printf("Bindning Successful\n");
+	if ((serverfd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+	}
+    printf("\nServerfd: %d", serverfd);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+	address.sin_port = htons(PORT);
+
+	if (bind(serverfd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		perror("bind failed");
+	} else {
+        printf("\nBinding successful\n");
     }
 
-    if(listen(sockfd, 10) == -1) {
-        printf("Listening error\n");
-    } else {
-        printf("Started listening on Port: %d\n", PORT);
+    if (listen(serverfd, 3) < 0) {
+		perror("listen");
+	} else {
+        printf("\nStarted listening on port: %d\n", PORT);
     }
 
-    int chat = accept(sockfd, (struct sockaddr *)&my_addr, (socklen_t *)&addrlen);
-    char buffer[1024] = {0};
-    int readval = read(chat, buffer, 1024);
-    printf("%s\n", buffer);
-    send(chat, "Hello, this is Om Shree", strlen("Hello, this is Om Shree, 2006077"), 0);
-    printf("Introduction initiated.\n");
+    if ((chat = accept(serverfd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+		perror("accept");
+	} else {
+        printf("\nAccept successful\n");
+    }
 
-    close(chat);
-    shutdown(sockfd, SHUT_RDWR);
-    return 0;
+    valread = read(chat, buffer, 1024);
+	printf("Message received from client:-\n%s\n", buffer);
+	send(chat, hello, strlen(hello), 0);
+	printf("Hello message sent from server\n");
+	
+
+	close(chat);
+	shutdown(chat, SHUT_RDWR);
+	return 0;
 }
