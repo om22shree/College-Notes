3165e3883a44a38a555f0db84a0d198b243ac85e
Om Shree <om22shree@gmail.com>
2022-08-18 20:31:50 +0530

    client updated

--- CN Lab/Day 2/client.c
+++ CN Lab/Day 2/client.c
@@ -21,6 +21,7 @@ void func(int sockfd) {
 			printf("Client Exit...\n");
 			break;
 		}
+		
 		bzero(buff, sizeof(buff));
 		read(sockfd, buff, sizeof(buff));
 		printf("From Server : %s", buff);
@@ -35,32 +36,26 @@ int main() {
 	int sockfd, connfd;
 	struct sockaddr_in servaddr, cli;
 
-	// socket create and verification
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
 		printf("socket creation failed...\n");
 		exit(0);
-	}
-	else
+	} else
 		printf("Socket successfully created..\n");
 	bzero(&servaddr, sizeof(servaddr));
 
-	// assign IP, PORT
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	servaddr.sin_port = htons(PORT);
 
-	// connect the client socket to server socket
 	if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) != 0) {
 		printf("connection with the server failed...\n");
 		exit(0);
-	}
-	else
+	} else
 		printf("connected to the server..\n");
 
-	// function for chat
 	func(sockfd);
-
-	// close the socket
 	close(sockfd);
+
+	return 0;
 }
