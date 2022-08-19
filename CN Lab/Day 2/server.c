e1ed64d796a5f29ef293e26553c083bdf21e357b
Om Shree <om22shree@gmail.com>
2022-08-19 13:24:01 +0530

    Day 2 UDP implementation

--- CN Lab/Day 2/server.c
+++ CN Lab/Day 2/server.c
@@ -1,79 +1,70 @@
+// Server side implementation of UDP client-server model
 #include <stdio.h>
-#include <netdb.h>
-#include <netinet/in.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
-#include <sys/socket.h>
 #include <sys/types.h>
-#define MAX 80
-#define PORT 8080
-#define SA struct sockaddr
-
-void func(int connfd) {
-	char buff[MAX];
-	int n;
-
-	for (;;) {
-		bzero(buff, MAX);
-		read(connfd, buff, sizeof(buff));
-        if (strncmp("exit", buff, 4) == 0) {
-			printf("Server Exit...\n");
-			break;
-		}
-		
-		printf("From client: %s\t To client : ", buff);
-		bzero(buff, MAX);
-		n = 0;
-		while ((buff[n++] = getchar()) != '\n')
-			;
-		write(connfd, buff, sizeof(buff));
-
-		if (strncmp("exit", buff, 4) == 0) {
-			printf("Server Exit...\n");
-			break;
-		}
-	}
-}
-
-
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+	
+#define PORT	 8080
+#define MAXLINE 100
+	
+// Driver code
 int main() {
-	int sockfd, connfd, len;
-	struct sockaddr_in servaddr, cli;
-
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd == -1) {
-		printf("socket creation failed...\n");
-		exit(0);
-	} else
-		printf("Socket successfully created..\n");
-	bzero(&servaddr, sizeof(servaddr));
-
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int sockfd;
+	char buffer[MAXLINE];
+	struct sockaddr_in servaddr, cliaddr;
+		
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+		
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+		
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
-
-	if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) {
-		printf("socket bind failed...\n");
-		exit(0);
-	} else
-		printf("Socket successfully binded..\n");
-
-	if ((listen(sockfd, 2)) != 0) {
-		printf("Listen failed...\n");
-		exit(0);
-	} else
-		printf("Server listening..\n");
-	len = sizeof(cli);
-
-	connfd = accept(sockfd, (SA*)&cli, &len);
-	if (connfd < 0) {
-		printf("server accept failed...\n");
-		exit(0);
-	} else
-		printf("server accept the client...\n");
-
-	func(connfd);
-	close(sockfd);
-
-	return 0;
+		
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 ) {
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+		
+    while(1) {
+        int len, n;
+        len = sizeof(cliaddr); //len is value/result
+	
+	    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
+			MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+			&len);
+	    buffer[n] = '\0';
+	    printf("Client : %s", buffer);
+        if (strncmp("exit", buffer, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+		bzero(buffer, MAXLINE);
+        n = 0;
+        printf("To client: ");
+        while ((buffer[n++] = getchar()) != '\n')
+            ;
+        sendto(sockfd, buffer, strlen(buffer),
+        MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
+        if (strncmp("exit", buffer, 4) == 0) {
+            printf("Server Exit...\n");
+            break;
+        }
+        bzero(buffer, MAXLINE);
+        n = 0;
+    }
+    close(sockfd);
+    return 0;
 }
