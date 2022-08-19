042b3594f7c2578032cfd21e0f489f9cace383ff
Om Shree <om22shree@gmail.com>
2022-08-19 13:27:24 +0530

    Day 2 final

--- CN Lab/Day 2/server.c
+++ CN Lab/Day 2/server.c
@@ -1,4 +1,3 @@
-// Server side implementation of UDP client-server model
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,13 +10,12 @@
 #define PORT	 8080
 #define MAXLINE 100
 	
-// Driver code
+
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
 	struct sockaddr_in servaddr, cliaddr;
 		
-	// Creating socket file descriptor
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
@@ -25,26 +23,20 @@ int main() {
 		
 	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
-		
-	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_family = AF_INET;
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
 		
-	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-			sizeof(servaddr)) < 0 ) {
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0 ) {
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
 		
     while(1) {
         int len, n;
-        len = sizeof(cliaddr); //len is value/result
+        len = sizeof(cliaddr);
 	
-	    n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-			MSG_WAITALL, ( struct sockaddr *) &cliaddr,
-			&len);
+	    n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, ( struct sockaddr*) &cliaddr, &len);
 	    buffer[n] = '\0';
 	    printf("Client : %s", buffer);
         if (strncmp("exit", buffer, 4) == 0) {
