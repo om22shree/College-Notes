6e13842bb7c79d6644cda82d488ef3327acc36cf
Om Shree <om22shree@gmail.com>
2022-08-25 22:18:50 +0530

    day 3 client 1 update

--- CN Lab/Day 3/client31.c
+++ CN Lab/Day 3/client31.c
@@ -11,15 +11,13 @@ int main() {
 	int i, sock, flag=0;
 	char buf[100], mes[100];
 	struct sockaddr_in server;
-	
-	// Create socket
+
 	sock = socket(AF_INET,SOCK_STREAM,0);
 
 	server.sin_family = AF_INET;
 	server.sin_addr.s_addr = inet_addr("127.0.0.1");
 	server.sin_port = 60018;
 
-	// Connect to remote server
 	connect(sock, (struct sockaddr *)&server, sizeof(server));
 
 	for(i=0; i < 100; i++) { 	
@@ -31,23 +29,19 @@ int main() {
 	while(flag == 0) {
 		for(i=0; i < 100; i++) buf[i] = '\0';
 		printf("\nString: ");
-		scanf("%[^\n]%*c", buf);
-		
-		// Send to server  
+		scanf("%[^\n]%*c", buf); 
 		send(sock, buf, 100, 0);
-		
-		for(i=0; i < 100; i++) buf[i] = '\0';
-		
-		// Receive a reply from the server
+		for(i=0; i < 100; i++) 
+			buf[i] = '\0';
 		recv(sock, buf, 100, 0);
 		printf("Server sent (of client 2): %s\n", buf);
+		
 		if(strcmp(buf,mes) == 0) {
 			flag = 1;
 			break;
 		}
 	}
 	
-	// close the socket	
 	close(sock);
 	return 0;
 }
