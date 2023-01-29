278ed9ac19bec931014507087d67d3542f579831
Om Shree <om22shree@gmail.com>
2023-01-29 23:09:11 +0530

    DA Day2 added

--- DA Lab/Day 2/code/q5.r
+++ DA Lab/Day 2/code/q5.r
@@ -1,3 +1,20 @@
-vec1 <- c(12, 15, 8, 2, 99, 77, 100)
+vec1 <- as.integer(strsplit(readline(), " ")[[1]])
 vec2 <- sort(vec1, decreasing = TRUE)
+print(vec2[2])
+
+bubble_sort_descending <- function(vec) {
+  n <- length(vec)
+  for (i in 1:(n-1)) {
+    for (j in 1:(n-i)) {
+      if (vec[j] < vec[j+1]) {
+        temp <- vec[j]
+        vec[j] <- vec[j+1]
+        vec[j+1] <- temp
+      }
+    }
+  }
+  return(vec)
+}
+
+vec2 <- (bubble_sort_descending(vec1))
 print(vec2[2])
