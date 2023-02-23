d7b40bf90fa9282cb6ab5ac820278f0a89fd1d05
Om Shree <om22shree@gmail.com>
2023-02-23 13:20:24 +0530

    updates in day 5

--- DA Lab/Day 5/q4.R
+++ DA Lab/Day 5/q4.R
@@ -1,2 +1,4 @@
 # Sum of rows across all the matrices :-
 print(rowSums(arr))
+print(apply(arr, 1, sum))
+print(apply(arr, 2, sum))
--- DA Lab/Day 5/q5.R
+++ DA Lab/Day 5/q5.R
@@ -1,7 +1,12 @@
 # Max and Min element in the array :-
+print(arr)
 
 # For max :-
 print(max(arr))
+print(apply(arr, 1, max))
+print(apply(arr, 2, max))
 
 # For min :-
 print(min(arr))
+print(apply(arr, 1, min))
+print(apply(arr, 2, min))
